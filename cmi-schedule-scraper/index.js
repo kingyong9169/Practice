771cad4c0287f45d4f57cf6053edabc130334503
동동이 <drdragon777@naver.com>
2021-10-09 21:30:50 +0900

    [FIX] (#3) - fix let -> const

--- cmi-schedule-scraper/index.js
+++ cmi-schedule-scraper/index.js
@@ -10,11 +10,11 @@ const crawl = async () => {
 
     let data = await page.evaluate(()=>{
         getData: function getData(){
-            let data = [];
-            let year = document.querySelectorAll(`#contents > div.academic_calendar > ul > li`);
+            const data = [];
+            const year = document.querySelectorAll(`#contents > div.academic_calendar > ul > li`);
             for(let i = 0; i < year.length; i++){
-                let monthData = [];
-                let month = year[i].querySelectorAll("div:nth-child(2) > div:nth-child(1) > ul > li");
+              const monthData = [];
+                const month = year[i].querySelectorAll("div:nth-child(2) > div:nth-child(1) > ul > li");
                 for(let j = 0; j < month.length; j++){
                     let date = month[j].querySelector("span").textContent;
                     let content = month[j].textContent;
