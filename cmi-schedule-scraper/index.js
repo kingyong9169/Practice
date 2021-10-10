ce6bdf878843d57fa6e18ff307c2104252e4688d
동동이 <drdragon777@naver.com>
2021-10-11 03:41:49 +0900

    [ADD] (#3) - array to object

--- cmi-schedule-scraper/index.js
+++ cmi-schedule-scraper/index.js
@@ -11,20 +11,49 @@ const crawl = async () => {
     let data = await page.evaluate(()=>{
         getData: function getData(){
             const data = [];
-            const year = document.querySelectorAll(`#contents > div.academic_calendar > ul > li`);
-            for(let i = 0; i < year.length; i++){
-              const monthData = [];
-                const month = year[i].querySelectorAll("div:nth-child(2) > div:nth-child(1) > ul > li");
-                for(let j = 0; j < month.length; j++){
-                    let date = month[j].querySelector("span").textContent;
-                    let content = month[j].textContent;
-                    content = content.slice(date.length, content.length);
-                    date = date.slice(1, date.length - 1);
-                    monthData.push([date, content]);
-                }
-                data.push(monthData);
+            const content = document.querySelectorAll(`#contents > div.academic_calendar > ul > li > div:nth-child(2) > div:nth-child(1) > ul > li`);
+            for(let i = 0; i < content.length; i++){
+                let date = content[i].querySelector("span").textContent;
+                let text = content[i].textContent;
+                text = text.slice(date.length, text.length);
+                date = date.replace(/[^0-9.]/g, "").replace(/\.{2,}/g, ".");
+                date = date.slice(0, date.length - 1).split('.');
+                data.push({...stringToDate(date), content: text});
             }
             console.log(data)
+        }
+        stringToDate: function stringToDate(arr){
+          const validDate = (i) => { return arr[i].length === 1 ? 0 + arr[i] : arr[i] }
+          switch(arr.length){
+            case 2:
+              return {
+                start_date: `2021-${validDate(0)}-${validDate(1)}`,
+                end_date: null,
+              };
+            case 3:
+              return {
+                start_date: `2021-${validDate(0)}-${validDate(1)}`,
+                end_date: `2021-${validDate(0)}-${validDate(2)}`
+              };
+            case 4:
+              return {
+                start_date: arr[0].length === 4 ? 
+                  (`${arr[0]}-${validDate(1)}-${validDate(2)}`) : 
+                  (`2021-${validDate(0)}-${validDate(1)}`),
+                end_date: arr[0].length === 4 ? 
+                  (`${arr[0]}-${validDate(1)}-${validDate(3)}`) :
+                  (`2021-${validDate(2)}-${validDate(3)}`)
+              };
+            case 5:
+              return {
+                start_date: arr[0].length === 4 ?
+                  (`${arr[0]}-${validDate(1)}-${validDate(2)}`) :
+                  (`2021-${validDate(0)}-${validDate(1)}`),
+                end_date: arr[0].length === 4 ?
+                  (`${arr[0]}-${validDate(3)}-${validDate(4)}`) :
+                  (`${arr[2]}-${validDate(3)}-${validDate(4)}`)
+              }
+          }
         }
         return getData();
     })
