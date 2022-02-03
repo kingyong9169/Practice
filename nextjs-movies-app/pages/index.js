dca74a19b5fbcac7b18976b544439219f7e0218c
동동이 <drdragon777@naver.com>
2022-02-04 04:53:23 +0900

    [ADD] - Add getServerSideProps for SSR

--- nextjs-movies-app/pages/index.js
+++ nextjs-movies-app/pages/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
+export default function Home({results}) {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async() => {
@@ -13,8 +13,7 @@ export default function Home() {
   return(
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => 
+      {results?.map((movie) => 
         <div className="movie" key = {movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -28,6 +27,9 @@ export default function Home() {
           padding: 20px;
           gap: 20px;
           text-align: center;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -45,4 +47,13 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
