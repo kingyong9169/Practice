ccea6d262d6b679b18ec7317d54d7865c704a0a4
동동이 <drdragon777@naver.com>
2022-02-24 04:37:30 +0900

    [ADD] - Add simple test code

--- react-test-simple-app/src/App.js
+++ react-test-simple-app/src/App.js
@@ -3,10 +3,35 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
+        <div>
+          <button
+            data-testid="minus-button"
+            onClick={() => setCounter((counter) => counter - 1)}
+            disabled={disabled}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCounter((counter) => counter + 1)}
+            disabled={disabled}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: 'blue' }}
+            onClick={() => setDisabled((prev) => !prev)}
+          ></button>
+        </div>
       </header>
     </div>
   );
